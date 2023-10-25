2f1a37cbeb07d28f8084f9f27f0579726c2a55ba
Gopher Kaseya <20kw174@esisalama.org>
2023-10-25 08:55:01 +0200

    ajout de deux table (maison en vente et maison en location)

---
+++ database/migrations/2023_10_25_064445_create_maison_vente.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maison_vente', function (Blueprint $table) {
+            $table->id();
+            $table->string('photoprincipal');
+            $table->string('photos');
+            $table->integer('prix');
+            $table->string('description');
+            $table->integer('nombre_piece');
+            $table->integer('nombre_vue');
+            $table->string('categorie');
+            $table->string('devise');
+            $table->string('commune');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maison_vente');
+    }
+};
